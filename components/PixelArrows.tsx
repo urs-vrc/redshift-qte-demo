3f83a890bcca8dc418880b0ce967a53ec689eb8c
Ayase Minori <ayane@duck.com>
2026-07-19 02:29:04 +0800

    Make arrows more legible
    Signed-off-by: Ayase Minori <ayane@duck.com>

--- components/PixelArrows.tsx
+++ components/PixelArrows.tsx
@@ -1,10 +1,10 @@
 const PixelArrow = ({ rotation }: { rotation: string }) => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style={{ transform: `rotate(${rotation})` }}>
-    <rect x="10" y="4" width="4" height="16" fill="currentColor" />
-    <rect x="6" y="8" width="4" height="4" fill="currentColor" />
-    <rect x="14" y="8" width="4" height="4" fill="currentColor" />
-    <rect x="2" y="12" width="4" height="4" fill="currentColor" />
-    <rect x="18" y="12" width="4" height="4" fill="currentColor" />
+    <rect x="8" y="4" width="8" height="4" fill="currentColor" />
+    <rect x="4" y="8" width="16" height="4" fill="currentColor" />
+    <rect x="0" y="12" width="24" height="4" fill="currentColor" />
+    <rect x="8" y="16" width="8" height="4" fill="currentColor" />
+    <rect x="8" y="20" width="8" height="4" fill="currentColor" />
   </svg>
 );
 
